f94762c058c49c02dcc3a799c1ed9e986f34efb8
Freckle87 <slepne098@gmail.com>
2026-06-16 18:29:49 +0300

    Проверка бд

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,12 @@ app.use((req, res, next) => {
 
 app.use(cors());
 app.use(express.json());
-
+console.log({
+  DB_HOST: process.env.DB_HOST,
+  DB_PORT: process.env.DB_PORT,
+  DB_NAME: process.env.DB_NAME,
+  DB_USER: process.env.DB_USER
+});
 // Подключение к БД
 const pool = new Pool({
     user: process.env.DB_USER || 'postgres',
