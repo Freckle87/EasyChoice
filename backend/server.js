fb82bf30b684ac213ce0972e76f1bd619631bdbe
Freckle87 <slepne098@gmail.com>
2026-06-16 20:31:40 +0300

    Ну работай

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,6 @@ const jwt = require('jsonwebtoken');
 const path = require('path');
 
 
-app.use(express.static(path.join(__dirname, 'frontend')));
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -27,6 +25,7 @@ app.use((req, res, next) => {
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'frontend')));
 console.log({
   DATABASE_URL: process.env.DATABASE_URL ? "SET" : "NOT SET",
   PORT: process.env.PORT
