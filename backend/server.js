b9995eb2babbf371025fdb8aab9e37f11d8e2a2d
Freckle87 <slepne098@gmail.com>
2026-06-16 20:06:13 +0300

    сделал так чтобы html работал

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,7 @@ app.use((req, res, next) => {
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static('frontend'));
 console.log({
   DATABASE_URL: process.env.DATABASE_URL ? "SET" : "NOT SET",
   PORT: process.env.PORT
